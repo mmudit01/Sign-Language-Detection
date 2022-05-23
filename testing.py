966945273f56e6529a2e5ecc9d8bb81e48eb31a1
Mudit Aggarwal <mmudit01@gmail.com>
2022-05-23 13:31:08 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import tensorflow as tf
 from tensorflow import keras
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 
